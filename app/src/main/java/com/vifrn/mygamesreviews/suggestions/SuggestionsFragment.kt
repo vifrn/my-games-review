169fe5d73a3e58abfaee7a502a5f3b81882e1bfa
Vinicius Franco <viniciusfrancoma09@hotmail.com>
2022-09-16 03:02:16 -0300

    Add Data binding

--- app/src/main/java/com/vifrn/mygamesreviews/suggestions/SuggestionsFragment.kt
+++ app/src/main/java/com/vifrn/mygamesreviews/suggestions/SuggestionsFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.vifrn.mygamesreviews.R
+import com.vifrn.mygamesreviews.databinding.FragmentSuggestionsBinding
 
 class SuggestionsFragment : Fragment() {
+
+    lateinit var binding : FragmentSuggestionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_suggestions, container, false)
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_suggestions, container, false)
+        return binding.root
     }
 }
